75d905a8ec17a79ce38f0a2be8bb1c9e267a161d
LFnast <luyuhao19@mails.ucas.ac.cn>
2023-04-17 21:21:46 +0800

    20230417

---
+++ src/include/proto.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 卢驭昊 on 2023/4/17.
+//
+
+#include "proto.h"
---
+++ src/include/proto.h
@@ -0,0 +1,17 @@
+#ifndef IPV4_PROTO_H
+#define IPV4_PROTO_H
+
+#define DEFAULT_MGROUP "224.2.2.2";
+const int default_port = 5000;
+
+const int rcv_channel = 100; // 100 is reserved for rcv channel
+const int list_channel = 0; // 0 is reserved for list channel
+const int min_channel = 1; // 1 is reserved for min channel
+const int max_channel = min_channel + rcv_channel - 1; // 99 is reserved for max channel
+
+class proto {
+
+};
+
+
+#endif //IPV4_PROTO_H
