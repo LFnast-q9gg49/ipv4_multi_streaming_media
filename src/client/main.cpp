7593008c266399f5c1cd08b1f8c0080d9d324893
LFnast <luyuhao19@mails.ucas.ac.cn>
2023-04-18 21:06:18 +0800

    20230418

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,9 +1,15 @@
 #include <cstdio>
 #include <cstdlib>
 #include <unistd.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <getopt.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <fcntl.h>
+
 
 #include "client.h"
 #include "../include/proto.h"
@@ -24,10 +30,10 @@ struct client::client_conf_st client_conf = {
 };
 
 static void usage() {
-    fprintf(stdout,"-M --mgroup  multicast group\n"
-                   "-P --port    port\n"
-                   "-p --player  named playe\n"
-                   "-H --help    help\n");
+    fprintf(stdout, "-M --mgroup  multicast group\n"
+                    "-P --port    port\n"
+                    "-p --player  named playe\n"
+                    "-H --help    help\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -40,17 +46,21 @@ int main(int argc, char *argv[]) {
      * */
     pid_t pid;
     int index, ret, sid;
+    struct ip_mreqn mreq{};
+    struct sockaddr_in laddr{};
+
+
     struct option arg_arr[] = {
             {"mgroup", 1, nullptr, 'M'},
             {"port",   1, nullptr, 'P'},
             {"player", 1, nullptr, 'p'},
             {"help",   0, nullptr, 'H'},
-            {nullptr,  0, nullptr,  0}
+            {nullptr,  0, nullptr, 0}
     };
 
     while (true) {
-        ret = getopt_long(argc, argv, "M:P:p:H", &arg_arr, &index);
-        if (ret < 0){
+        ret = getopt_long(argc, argv, "M:P:p:H", reinterpret_cast<const option *>(&arg_arr), &index);
+        if (ret < 0) {
             break;
         }
         switch (ret) {
@@ -79,7 +89,30 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    setsockopt();
+
+    if (inet_pton(AF_INET, client_conf.mgroup, &mreq.imr_multiaddr.s_addr) != 1) {
+        perror("inet_pton");
+        exit(1);
+    }
+    mreq.imr_address.s_addr = htonl(INADDR_ANY);
+    mreq.imr_ifindex = if_nametoindex("eth0");
+    if (mreq.imr_ifindex == 0) {
+        perror("if_nametoindex");
+        exit(1);
+    }
+
+    if (setsockopt(sid, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
+        perror("setsockopt");
+        exit(1);
+    }
+
+    laddr.sin_family = AF_INET;
+    laddr.sin_port = htons(client_conf.rcvport);
+    laddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(sid, reinterpret_cast<sockaddr *>(&laddr), sizeof(laddr)) < 0) {
+        perror("bind");
+        exit(1);
+    }
 
     pipe();
 
